5f58d6aa96163be7b85eede0051fdc80ad77b0e6
govindarajmrp@gmail.com <govindarajmrp@gmail.com>
2019-12-28 00:00:11 +0530

    SignUpForm1

--- src/Components/SignUpForm.js
+++ src/Components/SignUpForm.js
@@ -36,14 +36,7 @@ const SignUpForm = (props) => {
         <Label for="exampleText">Text Area</Label>
         <Input type="textarea" name="text" id="exampleText" />
       </FormGroup>
-      <FormGroup>
-        <Label for="exampleFile">File</Label>
-        <Input type="file" name="file" id="exampleFile" />
-        <FormText color="muted">
-          This is some placeholder block-level help text for the above input.
-          It's a bit lighter and easily wraps to a new line.
-        </FormText>
-      </FormGroup>
+       
       <FormGroup tag="fieldset">
         <legend>Radio Buttons</legend>
         <FormGroup check>
