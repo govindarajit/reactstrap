98fa721a3e7e5b95548be3342a236cce588dd6c0
govindarajmrp@gmail.com <govindarajmrp@gmail.com>
2020-01-01 23:12:25 +0530

    Cards added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-import DropDowns from "./Components/DropDowns";
+import Cards from "./Components/Cards";
 export default class App extends Component {
   render() {
     return (
       <div>
-        <DropDowns/>
+        <Cards/>
       </div>
     )
   }
--- src/Components/DropDowns.js
+++ src/Components/DropDowns.js
@@ -1,27 +1,19 @@
-import React, { useState } from 'react';
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import React from 'react';
+import { UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
-const DropDowns = (props) => {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-
-  const toggle = () => setDropdownOpen(prevState => !prevState);
-
+export default function Example () {
   return (
-    <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+    <UncontrolledDropdown>
       <DropdownToggle caret>
         Dropdown
-        </DropdownToggle>
+      </DropdownToggle>
       <DropdownMenu>
         <DropdownItem header>Header</DropdownItem>
-        <DropdownItem>Some Action</DropdownItem>
-        <DropdownItem disabled>Action (disabled)</DropdownItem>
+        <DropdownItem disabled>Action</DropdownItem>
+        <DropdownItem>Another Action</DropdownItem>
         <DropdownItem divider />
-        <DropdownItem>Foo Action</DropdownItem>
-        <DropdownItem>Bar Action</DropdownItem>
-        <DropdownItem>Quo Action</DropdownItem>
+        <DropdownItem>Another Action</DropdownItem>
       </DropdownMenu>
-    </Dropdown>
+    </UncontrolledDropdown>
   );
 }
-
-export default DropDowns;
