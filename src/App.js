2636ee2ccd175a79fff6fbe5b38ce21c5c634e59
govindarajmrp@gmail.com <govindarajmrp@gmail.com>
2020-01-01 23:23:11 +0530

    overall added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 import PopOvers from "./Components/PopOvers";
+import Cards from "./Components/Cards";
+import Jumbotron from "./Components/Jumbotron";
+import ButtonGroup1 from "./Components/ButtonGroup1";
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Jumbotron/>
         <PopOvers/>
+        <Cards/>
+        <ButtonGroup1 />
       </div>
     )
   }
